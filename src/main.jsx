d720630714e3e147da38d3f1c7d5be14186fcc13
Fikri Rahmat Nurhidayat <fikrirnurhidayat@gmail.com>
2021-12-10 15:10:04 +0700

    ci: add base url on build

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import "./assets/css/main.css";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
+const BASE_URL = import.meta.env.BASE_URL;
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
